8e4babd79268dd5085e331aed0619683f7fb8a91
Esteban Cristóbal <ecristobalr@gmail.com>
2025-10-10 10:17:10 +0200

    feat: HAL explorer working well with GET requests to our kids repository

--- src/main/java/com/sagittec/lyra/members/api/repositories/Kid.java
+++ src/main/java/com/sagittec/lyra/members/api/repositories/Kid.java
@@ -1,10 +1,12 @@
 package com.sagittec.lyra.members.api.repositories;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import jakarta.persistence.Version;
 import lombok.AccessLevel;
@@ -23,13 +25,18 @@ import org.springframework.data.annotation.LastModifiedDate;
 class Kid {
 
     @Id
-    private UUID id;
+    @GeneratedValue(generator = "kids_seq")
+    @SequenceGenerator(name = "kids_seq", sequenceName = "KIDS_SEQ", allocationSize = 1, initialValue = 1)
+    @Column(name = "ID")
+    private int id;
 
     @Version
+    @Column(name = "OPTLOCK")
     @Setter(AccessLevel.NONE)
-    private Long version;
+    private int version;
 
     @LastModifiedDate
+    @Column(name = "LAST_MODIFIED_DATE")
     @Setter(AccessLevel.NONE)
     private LocalDateTime lastModifiedDate;
 
--- src/main/java/com/sagittec/lyra/members/api/repositories/KidsRepository.java
+++ src/main/java/com/sagittec/lyra/members/api/repositories/KidsRepository.java
@@ -1,7 +1,5 @@
 package com.sagittec.lyra.members.api.repositories;
 
-import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,4 +7,4 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @Repository("kids")
 public interface KidsRepository
-        extends JpaRepository<Kid, UUID> {}
+        extends JpaRepository<Kid, Integer> {}
