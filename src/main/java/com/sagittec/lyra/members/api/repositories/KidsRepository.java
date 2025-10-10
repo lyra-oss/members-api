b6cc5ea79875d6b1ebdfe958503c7b146d95491e
Esteban Cristóbal <ecristobalr@gmail.com>
2025-10-10 16:21:33 +0200

    style: Reworked repositories' annotations

--- src/main/java/com/sagittec/lyra/members/api/repositories/KidsRepository.java
+++ src/main/java/com/sagittec/lyra/members/api/repositories/KidsRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-@Transactional
 @Repository
+@Transactional
 public interface KidsRepository
         extends JpaRepository<Kid, Integer> {}
