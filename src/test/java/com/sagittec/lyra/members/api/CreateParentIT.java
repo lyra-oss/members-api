1a4c7e631775a392275d4b73503302f4ed12e6d1
Esteban Cristóbal <ecristobalr@gmail.com>
2025-10-12 10:45:11 +0200

    config: Added "embedded" profile to use H2 database instead of PostgreSQL

--- src/test/java/com/sagittec/lyra/members/api/CreateParentIT.java
+++ src/test/java/com/sagittec/lyra/members/api/CreateParentIT.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.params.provider.Arguments.arguments;
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@ActiveProfiles("embedded")
 @AutoConfigureMockMvc
 class CreateParentIT {
 
