4b80ec772c63eade514f4144e4d943d448dfa66e
Esteban Cristóbal <ecristobalr@gmail.com>
2025-10-17 14:53:59 +0200

    style: Cucumber test code refactoring

--- src/test/java/com/sagittec/lyra/members/api/RunCucumberTests.java
+++ src/test/java/com/sagittec/lyra/members/api/RunCucumberTests.java
@@ -1,8 +1,20 @@
 package com.sagittec.lyra.members.api;
 
+import io.cucumber.spring.CucumberContextConfiguration;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.Suite;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @Suite
 @IncludeEngines("cucumber")
-class RunCucumberTests {}
+class RunCucumberTests {
+
+    @CucumberContextConfiguration
+    @SpringBootTest
+    @AutoConfigureMockMvc
+    @ActiveProfiles("embedded")
+    static class CucumberSpringConfiguration {}
+
+}
