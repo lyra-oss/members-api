4d4cbfb5b326c9c2e1e1666f08305b3d61669884
Esteban Cristóbal <ecristobalr@gmail.com>
2026-07-10 14:40:00 +0200

    feat: Added paging capabilities to kids repository

--- src/main/java/edu/lyra/members/api/repositories/jpa/KidsRepository.java
+++ src/main/java/edu/lyra/members/api/repositories/jpa/KidsRepository.java
@@ -1,10 +1,11 @@
 package edu.lyra.members.api.repositories.jpa;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.ListPagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @Transactional
 public interface KidsRepository
-        extends CrudRepository<Kid, Integer> {}
+        extends CrudRepository<Kid, Integer>, ListPagingAndSortingRepository<Kid, Integer> {}
